b8498275aa9b58cb83e47b89a11fce04ba2f07bc
Amrithesh S <amritheshsiva@gmail.com>
2025-11-19 20:45:13 +0530

    changes

--- MySQL/Day 6/homework.sql
+++ MySQL/Day 6/homework.sql
@@ -2,3 +2,33 @@
 CREATE TABLE `school`.`movies` (`id` INT NOT NULL AUTO_INCREMENT , `title` VARCHAR(25) NOT NULL , `genre` VARCHAR(25) NOT NULL , `release_year` INT NOT NULL , `rating` DECIMAL NOT NULL , `box_office` BIGINT NOT NULL , PRIMARY KEY (`id`)) ENGINE = InnoDB;
 
 -- Inserting Value
+INSERT INTO movies
+VALUES
+(1,'Inception','Sci-Fi',2010,8.8,825),
+(2,'The Dark Knight','Action',2008,9.0,1004),
+(3,'Interstellar','Sci-Fi',2014,8.6,677),
+(4,'Joker','Drama',2019,8.4,1074),
+(5,'Dunkirk','War',2017,7.9,527);
+
+-- To show only the movie title and its box_office collection but rename the column box_office as Earnings (in Cr).
+SELECT title,box_office AS "Earnings (in Cr)"
+FROM movies;
+
+-- To display the movie titles and rename them as Movie Title, along with their genre as Category.
+SELECT title AS "Movie Title",genre AS "Category"
+FROM movies;
+
+-- To select all movies where genre is either 'Sci-Fi' or 'Action' using IN, and rename rating as IMDb Score.
+SELECT title,rating AS "IMDb Score" 
+FROM movies
+WHERE genre IN ('Sci-Fi','Action');
+
+-- To find movies released between 2008 and 2015 and rename the year column as Release.
+SELECT title, release_year AS "Release"
+FROM movies
+WHERE release_year BETWEEN 2008 AND 2015;
+
+-- To find out how many movies have a rating greater than 8.5 and rename the result as High Rated Movies
+SELECT COUNT(*) AS "High Rated Movies"
+FROM movies
+WHERE rating>8.5;

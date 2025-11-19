f02b325112dbad90645832da780997405a7b1041
Amrithesh S <amritheshsiva@gmail.com>
2025-11-19 17:58:51 +0530

    day 6

---
+++ MySQL/Day 6/homework.sql
@@ -0,0 +1,4 @@
+-- Table creation
+CREATE TABLE `school`.`movies` (`id` INT NOT NULL AUTO_INCREMENT , `title` VARCHAR(25) NOT NULL , `genre` VARCHAR(25) NOT NULL , `release_year` INT NOT NULL , `rating` DECIMAL NOT NULL , `box_office` BIGINT NOT NULL , PRIMARY KEY (`id`)) ENGINE = InnoDB;
+
+-- Inserting Value

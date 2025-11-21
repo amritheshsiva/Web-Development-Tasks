8f9b2ab530cd9269843e5154420fc43a5d54e431
Amrithesh S <amritheshsiva@gmail.com>
2025-11-21 17:45:06 +0530

    day 8

---
+++ MySQL/Day 8/classwork.sql
@@ -0,0 +1,20 @@
+-- Datatbase Creation
+CREATE DATABASE GroceryShop;
+-- Selecting database
+USE groceryshop;
+
+-- Creating Table
+CREATE TABLE products
+(product_id INT PRIMARY KEY AUTO_INCREMENT,
+ product_name VARCHAR(25),
+ price INT);
+
+-- To add a new column called category (text) to the same table.
+ ALTER TABLE products
+ADD category VARCHAR(25);
+
+-- To remove all items from the products table without deleting the table itself.
+TRUNCATE TABLE products;
+
+-- delete the entire database as it’s no longer needed.
+DROP DATABASE groceryshop;

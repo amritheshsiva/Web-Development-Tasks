87d8921b958737caa11422ce3af0834174f6fb95
Amrithesh S <amritheshsiva@gmail.com>
2025-11-26 20:08:07 +0530

    changes

--- MySQL/Day 11/homework.sql
+++ MySQL/Day 11/homework.sql
@@ -1,6 +1,24 @@
 -- Database creation
+CREATE DATABASE library_db;
+
 -- Selecting Database
+USE library_db;
+
 -- Table "categories" creating
--- Inserting values in "categories" table
+CREATE TABLE categories(
+    category_id INT PRIMARY KEY AUTO_INCREMENT,
+    category_name VARCHAR (20));
+
 -- Table "books" creating
--- Inserting values in "books" table
+CREATE TABLE books(
+    book_id INT PRIMARY KEY AUTO_INCREMENT,
+    book_name VARCHAR(30),
+    category_id INT,
+    FOREIGN KEY(category_id) REFERENCES categories(category_id),
+    INDEX(book_name)
+    );
+
+-- To show all indexes in the books table.
+
+SHOW INDEXES FROM books;
+

940f273c9636c991788e1ff7f39feffe96e70d57
Amrithesh S <amritheshsiva@gmail.com>
2025-12-01 17:59:56 +0530

    changes

--- Python/Day 1/homework.py
+++ Python/Day 1/homework.py
@@ -1,8 +1,20 @@
-price_rice=40
-price_sugar=40
-price_oil=130
-quant_rice=3
+import random
+price_rice=float(40)
+price_sugar=float(40)
+price_oil=float(130)
+quant_rice=float(3)
 quant_sugar=2.5
 quant_oil=1.8
-total_rice=
-
+total_rice=price_rice*quant_rice
+total_sugar=price_sugar*quant_sugar
+total_oil=price_oil*quant_oil
+print("Total price of rice is",total_rice)
+print("Total price of sugar is",total_sugar)
+print("Total price of oil is",total_oil)
+total=total_rice+total_sugar+total_oil
+print("Total bill is",total)
+print("Total bill as integer",int(total))
+print("Total billa as string",str(total))
+del_charge= random.randrange(5,10)
+final=total+del_charge
+print("Bill including delivery charge",final)

3b88b3e06179e10165920db2bedb5fd7c9646aba
Amrithesh S <amritheshsiva@gmail.com>
2025-12-03 20:14:54 +0530

    changes

---
+++ Python/test.py
@@ -0,0 +1,23 @@
+y=5
+x=5
+x+=5
+print(x)
+x-=5
+print(x)
+a=4
+b=4
+print(a==b)
+
+a=10
+b=4
+print(~b)
+
+
+
+
+
+# id is to check the memory location
+# is is used to check the memory location
+
+
+

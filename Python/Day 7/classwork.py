745b8f85674e6a0bb9bf3065183d18f7f6fa79c0
Amrithesh S <amritheshsiva@gmail.com>
2025-12-11 12:32:28 +0530

    changes

--- Python/Day 7/classwork.py
+++ Python/Day 7/classwork.py
@@ -1,13 +1,9 @@
 items = ["milk", "bread", "eggs"]
-
 def add_item(item):
     items.append(item)
-
 def remove_last_item():
     items.pop()
-
 display_item = lambda item: print(f"Item: {item}")
-
 def count_characters(items, index=0):
     if index == len(items):
         return 0

1769bb1e13befa386aa52c167d9ee1a028563d45
Amrithesh S <amritheshsiva@gmail.com>
2026-01-08 10:54:40 +0530

    changes

--- Django/practice/project2/app1/forms.py
+++
@@ -1,5 +0,0 @@
-from django import forms
-class LoginForm(forms.Form):
-    email = forms.CharField()
-    password = forms.CharField()
- 
--- Django/practice/project2/app1/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from .forms import LoginForm
-def greeting(request):
-    if request.method == 'POST':
-        form = LoginForm(request.POST)
-        return render(request,'form-data.html',{
-            'email': form['email'].value
-        })
-    return render(request,'index.html')

18e35c7888da7fa6bb45b86f17fb5d19cd0a49e5
Amrithesh S <amritheshsiva@gmail.com>
2026-02-01 19:29:51 +0530

    changes

--- React/D1/project_cw/src/App.js
+++ React/D1/project_cw/src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import images from "./images/profile.webp";
 
 function App() {
+  console.log("React app started")
+  const uname = "Jishnu"
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container d-flex justify-content-center align-items-center vh-100">
+      <div className="card text-center p-4">
+        <h2 style={{ color: "#1568e5", fontWeight: "bold" }}>Welcome to React Learning, {uname}</h2>
+
+        <div className="mt-3">
+          <img src={images} alt="Internal" className="img-fluid mb-3" style={{ width: "200px" }}/>
+          <img src="https://imgs.search.brave.com/NM8_wHLqdydUirjKv3WAG0emS3yZRQgn8yhxKy5_II0/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly90b3Bw/bmcuY29tL3VwbG9h/ZHMvcHJldmlldy9y/ZWFjdC1sb2dvLWlj/b24tMTE2MDkzNzQx/MjJkOXZrYnB0cWFw/LnBuZw" alt="External" className="img-fluid" style={{ width: "200px" }} />
+        </div>
+        
+        <p className="text-muted mt-3">This is your first card with images and styles!</p>
+      </div>
     </div>
   );
 }
-
 export default App;
